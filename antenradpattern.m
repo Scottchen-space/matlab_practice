e297ac2dbe11ec27657e0064da0ea8cfd8ed0b9b
Scottchen-space <samhchen2007@gmail.com>
2026-06-09 21:34:43 +0800

    add 3D plot prac

---
+++ antenradpattern.m
@@ -0,0 +1,17 @@
+clc;
+clear;
+close all;
+
+x = -8:0.5:8;
+y = -8:0.5:8;
+
+[xx, yy] = meshgrid(x,y);
+
+R = sqrt(xx.^2+yy.^2);
+R = R + eps;
+
+Z = sin(R)./R;
+
+surf(Z);
+colormap("jet");
+colorbar;
